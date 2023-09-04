a31733d0e87231dc9dd280e88e3dda31cae0986d
tjftjrgus <tjftjrgus123@gmail.com>
2023-09-04 12:33:43 +0900

    0904수엄

---
+++ javawork/NaverJavaProject/src/bit701/day0904/Ex1_While.java
@@ -0,0 +1,38 @@
+package bit701.day0904;
+
+public class Ex1_While {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 *  반복문에는 for,while,do-while 3가지가 있다
+		 *  for문은 주로 반복횟수가 정해져있는경우에 많이 사용
+		 *  while이나 do-while은 반복횟수가 졍해져있지 않은 경우에 많이 사용
+		 *  while 과 do-while 차이점은 조건은 먼저주느냐,나중에 주느냐(선조건,후조건)차이
+		 */
+		
+		int a=65;//대문자 A의 아스키코드값
+		while(a<=90)// 조건이 참인동안 실행:91이 되면 빠져나간다
+		{
+//			System.out.println((char)a);
+//			a++;
+			System.out.println((char)a++);
+		}
+		System.out.println();
+		System.out.println("빠져나온후에 a값"+a);
+
+		a=65;
+		while(true) {
+			System.out.println((char)a++);
+			if(a>90) {
+				break;
+			}
+		}
+		System.out.println();
+		a=65;
+		do {
+			System.out.println((char)a++);
+		}while(a<=90);//조건뒤에 반드시; 추가
+	}
+
+}
---
+++ javawork/NaverJavaProject/src/bit701/day0904/Ex2_While.java
@@ -0,0 +1,10 @@
+package bit701.day0904;
+
+public class Ex2_While {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
